7acf9068408f2cd44eaeada8b10e09b4ec728211
Tarun <happytarunsh2002@gmail.com>
2025-09-12 23:27:09 +0530

    Configure base path for GitHub Pages

--- cosco/vite.config.ts
+++ cosco/vite.config.ts
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   base: '/cosco/',
-  // plugins: [
-  //   tailwindcss(),
-  // ],
+  plugins: [
+    tailwindcss(),
+  ],
 })
