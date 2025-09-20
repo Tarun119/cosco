f752cce49048c3a8992b9346ecd67475eaba3196
Tarun <82997644+Tarun119@users.noreply.github.com>
2025-09-20 14:42:14 +0530

    Update vite.config.ts

--- cosco/vite.config.ts
+++ cosco/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/cosco/',
+  base: '/',
   plugins: [
     tailwindcss(),
   ],
